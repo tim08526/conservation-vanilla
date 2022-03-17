8221791ad0d2e4175c7cd6a4c5dc7972769d0dec
Tim2 <timothyktu@berkeley.edu>
2022-03-17 20:44:51 +0000

    update from leaderboard

--- conservation-5-ppo.py
+++ conservation-5-ppo.py
@@ -6,7 +6,7 @@ from stable_baselines3.common.evaluation import evaluate_policy
 r = 0.3
 K = 1
 env = gym.make("conservation-v5", r=r, K=K)
-model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="var/log/tensorboard/tim")
+model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=10000)
 mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=50)
 print("mean reward:", mean_reward, "std:", std_reward)
